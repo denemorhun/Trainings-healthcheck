49679f52e03f2d08839eb4b8c1abae27af056fb7
Denem Orhun <denemorhun@gmail.com>
2020-08-28 12:35:10 +0300

    Latest

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# Trainings/healthcheck
+# Healthcheck Endpoint
 
-Anyone (or any build server) that receives a copy of the project needs only to run the command:
 
-$ pip freeze > requirements.txt
- 
-# uvicorn app:app --reload --port 8080 --host 0.0.0.0
+Application url should return a greeting such as Hello:World on  as json when user opens a browser and navigates to http://localhost:8080,
+
+Application should provide a health endpoint (http://localhost:8080/healthz) that returns HTTP status (200 OK) which indicates health of the application and returns a valid json with the following information:
+- status: status of the app: online, success, OK, error, etc.
+- version: running application version (ex: 0.0.1). Version is stored in a file within the package. 
+- uptime: time duration or time stamp since the app is running (ex: running since YYYY-MM-DD hh:mm:ss) Example: When you open a browser and navigate to http://localhost:8080/healthz it should return something like:
+
+{
+    "status": 200,
+    "Server running since:": "2020/08/28, 11:51:46",
+    "Uptime in seconds": 380,
+    "Version": "0.1.4"
+}
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,22 +1,16 @@
 # author: Denem Orhun
-# version: 
-# date modified: 08/24/2020
+# date modified: 08/28/2020
 
 '''
-Using your favorite language (Go, Python, Java, Scala, Bash, etc.), create a hello world web application API that listens on port 8080 and greets a user with Hello! and exposes a health status endpoint.
+Basic app that returns the health of an endpoint. 
 
-Application url should return a greeting such as Hello! as json or plain text (ex: when you open a browser and navigate to http://localhost:8080, it should return Hello! plain text.)
-Application should provide a health endpoint (http://localhost:8080/healthz) that returns HTTP status (200 OK) which indicates health of the application and returns a valid json with the following information:
-status: status of the app: online, success, OK, error, etc.
-version: running application version (ex: 0.0.1)
-uptime: time duration or time stamp since the app is running (ex: running since YYYY-MM-DD hh:mm:ss) Example: When you open a browser and navigate to http://localhost:8080/healthz it should return:
+Open a browser and navigate to http://localhost:8080/healthz it should return:
 {
   "status": "OK",
   "version": "0.0.1",
   "uptime": "up since 2020-08-04 08:00:05"
 }
 '''
-
 
 import uvicorn
 
@@ -50,7 +44,7 @@ def get_health():
     version = fp.readline()
     return {
             "status": status.HTTP_200_OK,
-            "Server running since:":server_start_time.strftime("%m/%d/%Y, %H:%M:%S"),
+            "Server running since:":server_start_time.strftime("%Y/%m/%d, %H:%M:%S"),
             "Uptime in seconds":time_running.seconds,
             "Version":version
             }
